cc46979e48c7a573cac4e84e2d693e2af626a2c9
LPR7124 <liviapr@al.insper.edu.br>
2025-10-19 15:28:45 -0300

    tent_2_question_1_resolucao

--- funcoes.py
+++ funcoes.py
@@ -1,9 +1,9 @@
 def define_posicoes(linha, coluna, orientacao, tamanho):
     pos_inicial = [linha, coluna]
     opcoes = []
-    for i in tamanho:
+    for i in range(tamanho):
         if orientacao == "vertical": #para pegar a vertical: mesma coluna e linha diferente:
             opcoes.append([linha+i,coluna])
         elif orientacao == "horizontal": # II II II horizontal: coluna diferente e mesma linha:
-             opcoes.append([linha,coluna+i])
-             return opcoes
+            opcoes.append([linha,coluna+i])
+    return opcoes
