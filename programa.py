0aac381bfdcee197b85e0bff798b65f18369b0f2
LPR7124 <liviapr@al.insper.edu.br>
2025-10-21 12:43:12 -0300

    tente_2_question_7_solucao

--- programa.py
+++ programa.py
@@ -5,24 +5,24 @@ frota = {}
 tipos_de_navios = { 
     "porta-aviões": (4,1),
     "navio-tanque": (3,2),
-    "contratorpedeiro": (2,3),
-    "contratorpedeiro": (1,4)
+    "contratorpedeiro": (2, 3),
+    "submarino": (1, 4)
 }
 for nome, (tamanho, quantidade) in tipos_de_navios.items():
     for i in range(quantidade):
         print(f"Insira as informações referentes ao navio {nome} que possui tamanho {tamanho}")
-        posicao_valida = False #antes de começar o loop, fala que é False 
-        while posicao_valida == False:
+        posicao_certa = False #antes de começar o loop, fala que é False 
+        while not posicao_certa:
             linha = int(input("Linha: "))
             coluna = int(input("Coluna: "))
             if nome != "submarino":
                 orienta = int(input("[1] Vertical [2] Horizontal >"))
-                orientacao = "horizontal" if orienta == 1 else "horizontal"
+                orientacao = "vertical" if orienta == 1 else "horizontal"
             else: 
                 orientacao = "horizontal"
             if posicao_valida(frota, linha, coluna, orientacao,tamanho):
                 frota = preenche_frota(frota, nome, linha, coluna, orientacao,tamanho)
-                posicao_valida = True # condição de ser True
+                posicao_certa = True # condição de ser True
             else:
                 print("Esta posição não está válida!")
-print("Esta posição não está válida!")
+print(frota)
